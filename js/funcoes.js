38899cb3825f3d8637eea203c4afaca2edb237df
Felipe Prodossimo <felipe.profissional4@gmail.com>
2021-04-07 23:21:04 -0300

    Conversor atualizado!

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,4 +1,4 @@
-let valorEmDolarTexto = prompt("Qual o valor em dolár que você deseja converter?");
+/*let valorEmDolarTexto = prompt("Qual o valor em dolár que você deseja converter?");
 console.log( typeof valorEmDolarTexto);
 // string = texto
 let valorEmDolarNumero = parseFloat(valorEmDolarTexto);
@@ -12,4 +12,12 @@ let valorEmReal = valorEmDolarNumero * 5.77;
 let valorEmRealFixado = valorEmReal.toFixed(2);
 
 alert (valorEmRealFixado);
+*/
 
+//document.body.style.background = 'black'
+
+function Converter(){
+   let campoPreco = document.querySelector("#inputNumber").value;
+   let valorEmReal = campoPreco * 5.77;
+   document.write("Valor total é " + valorEmReal);
+}
